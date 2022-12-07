ddbbd080c4056c82c7d993ded128f11aff2fd49b
cuanaway <cuanaway@gmail.com>
2022-12-07 07:37:59 +0100

    Deploy gh-pages

--- _next/static/chunks/webpack-eeddb425db1b4158.js
+++ _next/static/chunks/webpack-eeddb425db1b4158.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "66cdc5659c9ae033" + ".css";
+/******/ 			return "static/css/" + "b6a7d881ee8ac1d4" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
